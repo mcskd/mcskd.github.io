8b041cba395331347c2d50f0c4a9eb8e81895b73
mcskd <ksm.aomr.hcnh@gmail.com>
2022-05-03 13:58:58 +0900

    a

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const humberger = document.querySelector(".humberger");
+const nav_menu = document.querySelector(".open_links");
 
-
-humberger.addEventListener("click", () => {
+  humberger.addEventListener("click", () => {
   humberger.classList.toggle("active");
+  nav_menu.classList.toggle("active");
 })
+
+document.querySelectorAll(".links_item").forEach(n => n.addEventListener("click", () => {
+  humberger.classList.remove("active");
+  nav_menu.classList.remove("active");
+}))
+
