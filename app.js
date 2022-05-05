8fa7d9ed2a238806cabbc27936a0a8f21db244e8
mcskd <ksm.aomr.hcnh@gmail.com>
2022-05-05 21:52:09 +0900

    a

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const humberger = document.querySelector(".humberger");
+const humburger = document.querySelector(".humburger");
 const nav_menu = document.querySelector(".open_links");
 
-  humberger.addEventListener("click", () => {
-  humberger.classList.toggle("active");
+//ハンバーガーメニューを開く時の処理
+  humburger.addEventListener("click", () => {
+  humburger.classList.toggle("active");
   nav_menu.classList.toggle("active");
+  
 })
 
-document.querySelectorAll(".links_item").forEach(n => n.addEventListener("click", () => {
-  humberger.classList.remove("active");
+//ハンバーガーメニューを閉じる時の処理
+document.querySelectorAll('.open_links').forEach(n => n.addEventListener("click", () => {
+  humburger.classList.remove("active");
   nav_menu.classList.remove("active");
 }))
 
-
-document.querySelectorAll(".links_item").forEach(n => window.addEventListener("scroll", () => {
-  humberger.classList.remove("active");
-  nav_menu.classList.remove("active");
+document.querySelectorAll('.open_links').forEach(n => n.addEventListener("click", (e) => {
+  if (!e.target.closest('.links_item')) {
+    humburger.classList.remove("active");
+  }
 }))
-
