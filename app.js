89acee8154fcba26294fff32f3e60d9799509993
mcskd <ksm.aomr.hcnh@gmail.com>
2022-05-11 11:48:12 +0900

    z

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const humburger = document.querySelector(".humburger");
+const menu = document.query
 const nav_menu = document.querySelector(".open_links");
+const logo = document.getElementById("logo_phrase");
+const social = document.getElementById("social");
+const button = document.getElementById("nav_button");
 
 //ハンバーガーメニューを開く時の処理
   humburger.addEventListener("click", () => {
   humburger.classList.toggle("active");
   nav_menu.classList.toggle("active");
-  
+  logo.classList.toggle('active');
+  social.classList.toggle('active');
+  button.classList.toggle('active');
 })
+
 
 //ハンバーガーメニューを閉じる時の処理
 document.querySelectorAll('.open_links').forEach(n => n.addEventListener("click", () => {
   humburger.classList.remove("active");
   nav_menu.classList.remove("active");
+  logo.classList.remove('active');
+  social.classList.remove('active');
+  button.classList.remove('active');
 }))
 
 document.querySelectorAll('.open_links').forEach(n => n.addEventListener("click", (e) => {
